6bdcc8914c14405c2ff682671257bffc9f445fb1
poommooq <poommooq@gmail.com>
2017-06-17 01:44:06 +0700

    need to switch bash version

--- brew.sh
+++ brew.sh
@@ -21,6 +21,13 @@ then
 
   # Install from Brewfile
   brew bundle
+
+  # Switch to using brew-installed bash as default shell
+  if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
+   echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
+   chsh -s /usr/local/bin/bash;
+  fi;
+  
 else
   # Make sure we’re using the latest Homebrew.
   brew update
@@ -46,12 +53,6 @@ fi
 # brew install bash
 # brew tap homebrew/versions
 # brew install bash-completion2
-
-# Switch to using brew-installed bash as default shell
-# if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
-#  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
-#  chsh -s /usr/local/bin/bash;
-# fi;
 
 # Install `wget` with IRI support.
 # brew install wget --with-iri
